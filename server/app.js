2e4e6efe71e094b7ab437d3676d4548a0a98bf94
5nahalf <5nahalf@comcast.net>
2015-05-05 12:06:12 -0500

    save for code 500

--- server/app.js
+++ server/app.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.use("/", index);
-module.exports = app;
+
 var app = express();
 var index = require("./routes/index");
 
@@ -12,3 +10,6 @@ var server = app.listen(3000, function(){
     var port = server.address().port;
     console.log("listenin' on port: ", port);
 });
+app.use(bodyParser.json());
+app.use("/", index);
+module.exports = app;
