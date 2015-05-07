4b65bdc3b9ac017e1367bfd2e9f68732b8e28483
5nahalf <5nahalf@comcast.net>
2015-05-06 21:58:16 -0500

    change colors

--- server/app.js
+++ server/app.js
@@ -3,12 +3,7 @@ var bodyParser = require("body-parser");
 var app = express();
 var index = require("./routes/index");
 app.use(bodyParser.json());
-//app.use("/", index);
 
-//var server = app.listen(3000, function(){
-//    var port = server.address().port;
-//    console.log("listenin' on port: ", port);
-//});
 app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,8 @@
 var express = require("express");
 var router = express.Router();
 var mongoose = require('mongoose');
-// We use mongodb-uri (install it as a dependency with NPM) to format the URI
 var uriUtil = require('mongodb-uri');
-// I got this from Mongolab's GUI, yours will be different. I created a DB user "client" with pw of "client" for the purposes here, this definitely isn't secure.
 var uri = 'mongodb://client:client@ds031922.mongolab.com:31922/heroku_app36595117';
-// Finally, make the connection with the URI formatted for mongoose
 mongoose.connect(uriUtil.formatMongoose(uri));
 var Cat = mongoose.model("cat", {name:String});
 var path =require("path");
