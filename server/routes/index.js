c94a75dc1f5b7f4a21b9f88d10bc3a4df0eeca78
5nahalf <5nahalf@comcast.net>
2015-05-26 14:11:40 -0500

    not sure what im doing

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,42 @@ var mongoose = require('mongoose');
 var uriUtil = require('mongodb-uri');
 var uri = 'mongodb://client:client@ds031922.mongolab.com:31922/heroku_app36595117';
 mongoose.connect(uriUtil.formatMongoose(uri));
-var Cat = mongoose.model("cat", {name:String});
+
 var path =require("path");
+var Cat = mongoose.model("cat", {name:String});
+var Song = mongoose.model("data", {name:String, song:String, lyrics:String});
+
+router.post("/add", function(request, response, next){
+    var music = new Song({name: request.body.name, song: request.body.song, lyrics: request.body.lyrics});
+    music.save(function(err){
+        if(err) console.log("meow %s", err);
+        response.send(music.toJSON());
+        //next();
+    });
+});
+router.post('/remove', function(request, response, next) {
+    var music = Song.find({name: request.body.name, song: request.body.song, lyrics: request.body.lyrics});
+    music.remove(function(err) {
+        if (err) console.log('Error when removing cat: %s', err);
+        //next();
+    });
+});
+router.get("/musicdata", function(request, response, next){
+    return Song.find({}).exec(function(err, musics){
+        if(err) throw new Error(err);
+        response.send(JSON.stringify(musics));
+        //next();
+    });
+});
+
+router.get("/*", function(req, res, next) {
+    console.log("ayo for yayo");
+    var file = req.params[0] || "views/index.html";
+    res.sendFile(path.join(__dirname, "../public", file));
+    //next();
+});
+
+//fasdfsdfasdfasd
 router.post("/add", function(request, response, next){
     var kitty = new Cat({name: request.body.name});
     kitty.save(function(err){
@@ -35,5 +69,4 @@ router.get("/*", function(req, res, next) {
     res.sendFile(path.join(__dirname, "../public", file));
     //next();
 });
-
 module.exports = router;
