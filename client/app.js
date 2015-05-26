bdcf63242f0ec2535c1dc382737f733dede04748
5nahalf <5nahalf@comcast.net>
2015-05-26 14:44:58 -0500

    dfsga

--- client/app.js
+++ client/app.js
@@ -35,10 +35,10 @@ app.controller("DataController", ["$scope", "$http", function($scope, $http){
         })
     };
     $scope.add = function(data){
-        return $http.post("/madd", data).then(fetchMusic);
+        return $http.post("/add", data).then(fetchMusic);
     };
     $scope.remove = function(data) {
-        return $http.post('/mremove', data).then(fetchMusic);
+        return $http.post('/remove', data).then(fetchMusic);
     };
     fetchMusic();
 }]);
