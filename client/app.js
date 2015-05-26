0c316673c0073865bbbd14c7ab6a19bf27155f20
5nahalf <5nahalf@comcast.net>
2015-05-26 14:23:45 -0500

    grunted

--- client/app.js
+++ client/app.js
@@ -21,3 +21,24 @@ app.controller("IndexController", ["$scope", "$http", function($scope, $http){
     fetchCats();
 }]);
 
+app.controller("DataController", ["$scope", "$http", function($scope, $http){
+    $scope.music = {};
+    $scope.musics = [];
+    var fetchMusic = function(){
+        return $http.get("/musicdata").then(function(response){
+            if(response.status !== 200){
+                throw new Error("failed to fetch muisc from the api");
+            }
+            $scope.music = {};
+            $scope.musics = response.data;
+            return response.data;
+        })
+    };
+    $scope.add = function(data){
+        return $http.post("/madd", data).then(fetchMusic);
+    };
+    $scope.remove = function(data) {
+        return $http.post('/mremove', data).then(fetchMusic);
+    };
+    fetchMusic();
+}]);
