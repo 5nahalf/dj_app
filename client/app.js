6838a4d073034bd15d746b3dd7ab2e01700a6c59
5nahalf <5nahalf@comcast.net>
2015-05-27 07:51:20 -0500

    morning ya'll

--- client/app.js
+++ client/app.js
@@ -47,13 +47,8 @@ app.controller("DataController", ["$scope", "$http", function($scope, $http){
 $(document).ready(function() {
     $('.tabs .tab-links a').on('click', function(e)  {
         var currentAttrValue = $(this).attr('href');
-
-        // Show/Hide Tabs
         $('.tabs ' + currentAttrValue).show().siblings().hide();
-
-        // Change/remove current tab to active
         $(this).parent('li').addClass('active').siblings().removeClass('active');
-
         e.preventDefault();
     });
 });
